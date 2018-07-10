3d4f6ba4a73379785ab7fb68f93437b13fc05703
LukeMarkwordt <32344255+LukeMarkwordt@users.noreply.github.com>
2018-07-10 15:24:32 -0600

    Single GUI object

--- Assessment.java
+++ Assessment.java
@@ -1,82 +1,58 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
+
  * To change this template file, choose Tools | Templates
+
  * and open the template in the editor.
+
  */
+
 package codhisattva;
 
+
+
 /*
-* File: Assessment.java
-* Author: Daniel John
-* Date: June 28, 2018
-* Purpose: This class contains the functionality to store the various question
-* types and to hold the data that will be used to create the various tests.
+
+* File: Codhisattva.java
+
+* Author: Josh Rakow
+
+* Date: June 27, 2018
+
+* Purpose: This program serves to test the InstructionModule and ErrorIDQuestion
+
+* classes and their methods.
+
 */
 
+
+
 /**
+
  * Revision History
- * 28 June 2019 - Initial Assessment file created by Daniel with test arrays to 
- * test the various method calls against fake data
- * 
+
+ * 27 June 2019 - File created by Josh
+
+ * 29 June 2019 - Merged Daniel main.java tests into this file and removed the
+
+ * main now that our GitHub is up and running
+
+ * 10 July 2018 Luke Markwordt removed initial test code and place a single GUI object to run the program
+
  */
-public class Assessment {
-    
-    /* 
-    * Notes to self: 
-    * 1. Does this need a constructor built at some point?
-    * 2. Do the index values for questions need to start at 0 when initialized?
-    */
-    
-    // Create arrays to store the various question types using class objects
-    private CodeTracingQuestion[] codeTracingQuestions = new CodeTracingQuestion[25];
-    private ErrorIDQuestion[] errorIDQuestions;
-    //private MultipleChoiceQuestion[] multipleChoiceQuestions;
-    
-    
-    /*
-    * Theoretical method used to fill each appropriate test.  Will likely
-    * change as progress on the program continues.
-    */
-    public void fillTest()
-    {
-        String[] feedbackQuestionOne = {"Incorrect", "Correct"};
-        CodeTracingQuestion codeTracingQuestion1 = new CodeTracingQuestion(
-                "what would you do for a Klondike bar?", 0,0,feedbackQuestionOne);
-        codeTracingQuestions[0] = codeTracingQuestion1;
-    }
-    
-   // Variables local to this class to track the question number a user is on
-    private int codeTracingQuestionIndex, errorIDQuestionIndex, 
-            multipleChoiceQuestionIndex = 0;
-    
-    /**
-     * Method to display code tracing questions and increment the index once a
-     * question has been completed
-     */
-    public void displayCodeTracingQuestions() 
-    {
-        codeTracingQuestions[codeTracingQuestionIndex].display();
-        codeTracingQuestionIndex++;
-    }
-    
-    /**
-     * Method to display error ID questions and increment the index once a
-     * question has been completed
-     */
-    public void displayErrorIdQuestions() 
-    {
-    //    errorIDQuestions[errorIDQuestionIndex].display();
-        errorIDQuestionIndex++;
-    }
-    
-    /**
-     * Method to display multiple choice questions and increment the index once a
-     * question has been completed
-     */
-    public void displayMultipleChoiceQuestions() 
-    {
-    //    multipleChoiceQuestions[multipleChoiceQuestionIndex].display();
-        multipleChoiceQuestionIndex++;
-    }
+
+
+
+public class Codhisattva {
+
+
+
+    public static void main(String[] args) {
         
+        new JavaTutorialGUI();
+    }
+
+    
+
 }
